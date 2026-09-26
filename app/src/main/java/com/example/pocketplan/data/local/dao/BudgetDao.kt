e7b2383767394f9c9a648a453d5669d672a7482f
subham <98615013subham@gmail.com>
2026-09-26 11:49:50 +0530

    Complete Budget DAO

--- app/src/main/java/com/example/pocketplan/data/local/dao/BudgetDao.kt
+++ app/src/main/java/com/example/pocketplan/data/local/dao/BudgetDao.kt
@@ -1,4 +1,23 @@
 package com.example.pocketplan.data.local.dao
 
-class BudgetDao {
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import com.example.pocketplan.data.local.entity.Budget
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface BudgetDao {
+
+    @Query(
+        """
+        SELECT * FROM budgets
+        WHERE month = :month
+        LIMIT 1
+        """
+    )
+    fun getBudgetForMonth(month: String): Flow<Budget?>
+
+    @Upsert
+    suspend fun upsertBudget(budget: Budget)
 }
